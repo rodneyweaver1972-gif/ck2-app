a47a7b3f20ee69907aa4916fe13c19dd33f00742
rodneyweaver1972-gif <rodneyweaver1972@gmail.com>
2025-10-03 14:42:58 -0500

    add tailwind and postcss configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
 // tailwind.config.ts
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
